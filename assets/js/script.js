a21cc5d16bd48e51fa08c44134458c18e93e445d
stankajofre <157607276+stankajofre@users.noreply.github.com>
2024-04-09 22:26:24 -0400

    csmbia el tipo del input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-4905856019427443
+let token = 4905856019427443;
 
 //dom
 $(document).ready(function () {
@@ -8,7 +8,31 @@ $(document).ready(function () {
     botonDeBusqueda.on("click", function () {
         let valorDelImput = elementoInputDelHtml.val()
 
+        //validar que el valor de imput solo sea numero
+        if (valorDelImput && !isNaN(valorDelImput)) {
+            console.log("El imput no esta vacio")
+
+            consultarAlaApi(valorDelImput);
+        } else {
+            console.log("El input esta vacio")
+        }
+
     });
+
+    function consultarAlaApi(idDelSuperheroe) {
+        let url = `https://www.superheroeapi.com/api.php/${token}/${idDelSuperheroe}`
+
+        $.ajax({
+            url: url,
+            method: "get",
+            dataTyoe: "json",
+            success: function (resultado) {
+                console.log(resultado);
+
+            }
+
+        })
+    }
 
 
 

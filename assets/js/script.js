18d829477add7cadff90eed744ce078ecbe22a0f
stankajofre <157607276+stankajofre@users.noreply.github.com>
2024-04-09 21:51:53 -0400

    agrega scripts

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+4905856019427443
+
+//dom
+$(document).ready(function () {
+    const elementoInputDelHtml = $("#id_superheroe");
+    const botonDeBusqueda = $("#botonBusqueda");
+
+    botonDeBusqueda.on("click", function () {
+        let valorDelImput = elementoInputDelHtml.val()
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+

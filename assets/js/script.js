fdd8c6294ccc95d90d0ecb54938b3ec52dc166b5
stankajofre <157607276+stankajofre@users.noreply.github.com>
2024-04-10 19:47:10 -0400

    requerimiento 3 completado

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,9 +26,25 @@ $(document).ready(function () {
             url: url,
             method: "get",
             dataTyoe: "json",
-            success: function (resultado) {
-                console.log(resultado);
 
+            success: function (resultado) {
+                let templateDeTarjeta =
+                    `<div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="..." class="img-fluid rounded-start" alt="...">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">Nombre del Superheroe</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                        additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+                $("#resultado").append(templateDeTarjeta);
             }
 
         })
